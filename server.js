0bf51286f8c7059e3940ab2a3befa8a1f869fb47
Saravana <11138.saravanakrishnan@gmail.com>
2025-11-02 22:31:10 +0530

    Change 2 (after launch)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from CI/CD demo on Cloud Run!\n');
+  res.end('Change 2!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n');
 });
 
 server.listen(port, () => {
